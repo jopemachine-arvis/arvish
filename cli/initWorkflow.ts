2cd70ec95ed747fd2b30fd4e238ee76f8b6d22dd
jopemachine <jopemachine@naver.com>
2021-06-15 21:28:29 +0900

    Update init plugin logics

--- cli/initWorkflow.ts
+++ cli/initWorkflow.ts
@@ -1,8 +1,9 @@
 import fse from 'fs-extra';
 import path from 'path';
+import username from 'username';
 
 const mock = {
-  $schema: './workflow-schema.json',
+  $schema: 'https://raw.githubusercontent.com/jopemachine/arvis-extension-validator/master/workflow-schema.json',
   creator: '',
   name: '',
   enabled: true,
@@ -17,6 +18,14 @@ const mock = {
 export default async (name?: string) => {
   if (name) {
     mock.name = name;
+  } else {
+    mock.name = process.cwd().split(path.sep).pop() ?? '';
+  }
+
+  try {
+    mock.creator = await username() ?? '';
+  } catch (err) {
+    // skip initialize value
   }
 
   await fse.writeJSON(`${process.cwd()}${path.sep}arvis-workflow.json`, mock, {
