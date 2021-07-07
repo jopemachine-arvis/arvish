06def296eb00438e522146dc3e540a51f9a84e0f
jopemachine <jopemachine@naver.com>
2021-07-08 06:32:38 +0900

    update dependency

--- cli/prepublish.ts
+++ cli/prepublish.ts
@@ -2,16 +2,26 @@ import { validate } from 'arvis-extension-validator';
 import chalk from 'chalk';
 import findUp from 'find-up';
 import fse from 'fs-extra';
+import path from 'path';
 
-const extensionJsonPath = findUp.sync(['arvis-workflow.json', 'arvis-plugin.json'], { allowSymlinks: false, type: 'file'});
+const extensionJsonPath = findUp.sync(['arvis-workflow.json', 'arvis-plugin.json'], { allowSymlinks: false, type: 'file' });
 
 if (!extensionJsonPath) {
   console.error(chalk.red('Extension file not found!'));
   process.exit(1);
 }
 
+const extensionDir = path.dirname(extensionJsonPath);
+const pkgPath = path.resolve(extensionDir, 'package.json');
+const pkgExist = fse.pathExistsSync(pkgPath);
+const pkg = pkgExist ? fse.readJSONSync(pkgPath) : undefined;
 const info = fse.readJSONSync(extensionJsonPath!);
 const type = extensionJsonPath.endsWith('arvis-workflow.json') ? 'workflow' : 'plugin';
+
+if (pkg.version !== info.version) {
+  console.error('Make sure the package version is the same as the extension version.');
+  process.exit(1);
+}
 
 const { valid, errorMsg } = validate(info, type, { strict: true });
 
