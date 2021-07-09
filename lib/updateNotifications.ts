ce9f4eb5bfd4548fa5ebf17352ae01cbde641d38
jopemachine <jopemachine@naver.com>
2021-07-09 23:58:25 +0900

    bug fixed

--- lib/updateNotifications.ts
+++ lib/updateNotifications.ts
@@ -1,14 +1,16 @@
 import { checkUpdate } from 'arvis-notifier';
 import findUp from 'find-up';
 import fse from 'fs-extra';
+import path from 'path';
 
 export default async () => {
-  const path = await findUp(['package.json']);
-  if (!path) {
+  const currentDir = __dirname.split(path.sep).slice(0, -2).join(path.sep);
+  const pkgPath = await findUp('package.json', { cwd: currentDir });
+  if (!pkgPath) {
       throw new Error('Extension\'s package.json not found!');
   }
 
-  const pkg = await fse.readJSON(path);
+  const pkg = await fse.readJSON(pkgPath);
   const arvish = pkg.arvish || {};
 
   if (arvish.updateNotification !== false) {
